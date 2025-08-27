163f48124267685b32b709cde524ded87772aa90
William2403 <112112464+William2403@users.noreply.github.com>
2025-08-26 22:56:46 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#define led 2
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
